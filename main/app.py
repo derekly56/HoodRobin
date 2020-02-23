488cde007a7a58e4498a8ee4f254c6589e4b504c
EricEKim <eric.kim@colorado.edu>
2020-02-22 21:34:02 -0700

    front end

--- main/app.py
+++ main/app.py
@@ -1,7 +1,6 @@
 # flask app
 from flask import Flask, render_template, request
 import os
-from apis.stock_api import *
 
 app = Flask(__name__)
 #Setup base directory for images
@@ -11,12 +10,8 @@ app.config["IMAGE_UPLOADS"] = os.path.join('static','uploads')
 def homepage():
     return render_template("homepage.html")
 
-# @app.route('/v1/stock/information', methods=['GET'])
-# def information():
-#     # retrieve URL params
-#     name = request.args.get('name', default = 'Walmart', type = str)
-#     date = request.args.get('date', default = '2020-02-22', type = str)
-#     pass
+@app.route('/v1/stock/information', methods=['GET'])
+def information():
 
 #start flask apps
 app.run(debug=True)
