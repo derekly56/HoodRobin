06e62db3cff2269c833c410f36f46887568fd950
Justin <vuongjn@users.noreply.github.com>
2020-02-22 20:43:55 -0700

    Update app.py
    Added route - Gets 3 highest

--- main/app.py
+++ main/app.py
@@ -19,6 +19,45 @@ companies = {
 def homepage():
     return render_template("homepage.html")
 
+@app.route('/v1/stock/information/highest', methods=['GET'])
+def highest():
+    # retrieve URL params
+    name = request.args.get('name', default = '', type = str)
+    path_db = os.getcwd() + '/data/StocksDB.db'
+
+    if not name:
+        return 'Error --- Missing Field(s): Name', 404
+
+    if name in companies:
+        db = sqlite3.connect(path_db)
+        cursor = db.cursor()
+
+        ticker_name = companies[name]
+
+        search_query = """
+            select * from stock where ticker == '{0}' order by high DESC LIMIT 3;
+        """.format(ticker_name)
+
+        query = cursor.execute(search_query)
+        rows = query.fetchall()
+
+        ans = {}
+
+        for row in rows:
+            sub_ans = {}
+            sub_ans['ticker'] = row[0]
+            sub_ans['open'] = row[2]
+            sub_ans['close'] = row[3]
+            sub_ans['high'] = row[4]
+            sub_ans['low'] = row[5]
+            sub_ans['volume'] = row[6]
+
+            ans[row[1]] = sub_ans
+        return ans, 200
+
+    else:
+        return "Company information not currently tracked", 404
+
 @app.route('/v1/stock/information', methods=['GET'])
 def information():
     # retrieve URL params
